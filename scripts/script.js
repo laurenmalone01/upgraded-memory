e61683784e32c0456dda3dc63e6d390eada74c67
laurenmalone01 <97190412+laurenmalone01@users.noreply.github.com>
2022-02-07 23:15:27 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function(){ 
+  console.log("doc is ready");
+
+  $("#btnGetFacts").click(function (){
+
+    // if (typeof userName !== 'string'){
+    //   let coolFacts='not string';
+    //   return coolFacts;
+    // }
+  
+      let userName = $("#userName").val();
+      let coolFacts = generateCoolNameFacts(userName);
+      
+
+        $("#coolNameFactsOutput").html(coolFacts);
+        
+      });
+
+});
+
+function generateCoolNameFacts(userName){
+  let coolFacts='';
+
+
+  coolFacts = nameLength(userName);
+  coolFacts += "<h2>" + nameStart(userName) + "</h2>";
+  coolFacts += "<h2>" + nameEnd(userName) + "</h2>";
+  coolFacts += "<h2>" + nameReverse(userName) + "</h2>";
+
+  return coolFacts;
+}
+
+function nameLength(name){
+  return "Your name is " + name.length + " letters long";
+}
+
+function nameStart(name){
+  return "The First letter is " + name.charAt(0);
+}
+
+function nameEnd(name){
+  let lastIndex = name.length - 1
+  return "The Last letter of your name is " + name[lastIndex]
+}
+
+function nameReverse(name){
+  return "I put my thing down, flip it and reverse it: " + name.split("").reverse().join("");
+}
+
+
+
+//$ = jquery's way of naming stuff
+//$(...) = what's inside () is the "object" we are messing with
+// .ready() = method of $jQ
+// .ready(...) = argument/parameter passed to that method
+// function(){} in the method is an IIFE = Immediate Invoked F/n Expression
